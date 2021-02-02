64cd4f1a6d5fac360867df4e53ce4ae04f59e85e
mauropolizzi2 <mauropolizzi2@gmail.com>
2021-02-02 15:35:44 -0300

    practicaEjercitacion

---
+++ IgualdadProfunda/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IgualdadProfunda
+{
+    class Program
+    {
+        //Escribe una función igualdadProfunda que toma dos valores
+        //y retorne true solo si tienen el mismo valor o son objetos con las mismas propiedades,
+        //donde los valores de las propiedades sean iguales cuando comparadas con una llamada recursiva a igualdadProfunda.
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private static bool igualdadProfunda(object a, object b)
+        {
+            if (a == b) return true;
+
+            if (a == null || b == null) return false;
+
+            if (a.GetType() != typeof(object) || b.GetType() != typeof(object)) return false;
+
+            Array keyA = Array.CreateInstance(typeof(object), a[]);
+            Array keyB = Array.CreateInstance(typeof(object), b[]);
+
+            foreach(var key in keyA)
+            {
+                if (keyB.SetValue(key)) ;
+            }
+        }
+    }
+}
---
+++ PracticaEjercitacion/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PracticaEjercitacion
+{
+    // Escribe una función contarFs que tome un string como su único argumento y devuelva 
+    //un número que indica cuántos caracteres “F” en mayúsculas haya en el string.
+    //Despues, escribe una función llamada contarCaracteres que se comporte como contarFs,
+    //excepto que toma un segundo argumento que indica el carácter 
+    //que debe ser contado (en lugar de contar solo caracteres “F” en mayúscula). 
+    //Reescribe contarFs para que haga uso de esta nueva función.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(contarCaracteres("mmmm", "m"));
+            Console.ReadKey();
+        }
+
+        private static int contarCaracteres(string texto, string caracter)
+        {
+            
+
+            var count = 0;
+
+            for (int i = 0; i < texto.Length; i++)
+                
+            {
+                if (texto[i].ToString() == caracter)
+                {
+                    count += 1;
+                }
+            }
+
+
+            Console.WriteLine($"La palabra {texto}, contiene {count} veces {caracter}");
+            return count;
+        }
+
+        private static int contador(string texto)
+        {
+            return contarCaracteres(texto, "m");
+        }
+    }
+}
